2bf47541cc52fe7c3c632f8b5bcae9ee73551aca
SuperStar0718 <georgejohnson11305@gmail.com>
2023-08-12 08:33:57 +0900

    updated

--- resources/views/book/waste removals and european moves and storage/book1.blade.php
+++ resources/views/book/waste removals and european moves and storage/book1.blade.php
@@ -36,7 +36,7 @@
     <!--------------- end section 1 --------------->
 
     <!--------------- section 2 --------------->
-    <div class="container-content book_now_btn" style="margin-bottom: -120px;">
+    <div class="container-content book_now_btn display-none-sm " style="margin-bottom: -120px;">
         <div class="moving-home bg-white position-relative p-5">
             <div class="row">
                 <div class="col-md-7">
